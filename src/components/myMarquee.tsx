fa572a4b85b33957b04bf2151e62abf5a4fa6a9c
Jerald Lashy Jeffery <jeraldjeffreylashie@gmail.com>
2023-03-27 23:04:29 +0200

    First Commit

---
+++ src/components/myMarquee.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Marquee from "react-fast-marquee";
+
+export default function MyMarquee() {
+  return (
+    <Marquee>
+      <h3 className="font-light text-orange-600 text-xl uppercase tracking-wider">
+        Education, Travel, Business, Technology and Leisure
+      </h3>
+    </Marquee>
+  );
+}
